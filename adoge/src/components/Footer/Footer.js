a3c100e9b9d7aa3dc116eba4c37b5c6fb70ce4bc
gretoide <gretaxpiacentini@gmail.com>
2025-06-27 11:02:01 -0300

    Desktop version actualizada

--- adoge/src/components/Footer/Footer.js
+++ adoge/src/components/Footer/Footer.js
@@ -3,11 +3,15 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} AdoGe. Todos los derechos reservados.</p>
+    <div className="footer-copyright">
+      <p>&copy; {new Date().getFullYear()} AdoGe. Todos los derechos reservados.</p>
+    </div>
     <div className="footer-links">
-      <a href="#about">Sobre Nosotros</a>
-      <a href="#services">Servicios</a>
-      <a href="#contact">Contacto</a>
+      <a className="footer-link" href="#about">Sobre Nosotros</a>
+      <span className="footer-sep">|</span>
+      <a className="footer-link" href="#services">Servicios</a>
+      <span className="footer-sep">|</span>
+      <a className="footer-link" href="#contact">Contacto</a>
     </div>
   </footer>
 );
